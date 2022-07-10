b0c0afdd6d23bf23075ecb10a85595ec09efa30e
Tim Hunt <tnhunt@gmail.com>
2022-07-10 14:43:16 +0100

    rearranged some stuff and removed a few logging msgs

--- src/components/contextmenu/actions.py
+++ src/components/contextmenu/actions.py
@@ -1,98 +1,206 @@
 from gi.repository import Gtk
 from neil.com import com
 from neil.preset import Preset
-from neil.utils import gettext, prepstr, filenameify,show_machine_manual
+from neil.utils import gettext, prepstr, filenameify, show_machine_manual, is_root, clone_plugin, clone_plugin_patterns
+from zzub import Player
+import functools
 
 
-def on_popup_mute(widget, metaplugin):
-    com.get('neil.core.player').toggle_mute(metaplugin)
+def on_popup_mute_selected(widget, plugins):
+    com.get('neil.core.player').mute_group(plugins)
 
 
-def on_popup_solo(widget, metaplugin):
+def on_popup_mute(widget, plugin):
+    com.get('neil.core.player').toggle_mute(plugin)
+
+
+def on_popup_unmute_all(widget, plugin):
     player = com.get('neil.core.player')
-    if player.solo_plugin != metaplugin:
-        player.solo(metaplugin)
-    else:
-        player.solo(None)
-
-
-def on_popup_bypass(widget, metaplugin):
-    com.get('neil.core.player').toggle_bypass(metaplugin)
-
-
-def on_popup_show_params(widget, metaplugin):
-    com.get('neil.core.parameterdialog.manager').show(metaplugin, widget)
-
-
-def on_popup_show_attribs(widget, metaplugin):
-    dlg = com.get('neil.core.attributesdialog', metaplugin, self)
-    dlg.run()
-    dlg.destroy()
-
-
-def on_popup_show_presets(widget, metaplugin):
-    com.get('neil.core.presetdialog.manager').show(metaplugin, widget)
-
-
-def on_popup_rename(widget, metaplugin):
-    text = gettext(widget, "Enter new plugin name:", prepstr(metaplugin.get_name()))
-    if text:
-        player = com.get('neil.core.player')
-        metaplugin.set_name(text)
-        player.history_commit("rename plugin")
-
-
-def on_popup_delete(widget, metaplugin):
-    com.get('neil.core.player').delete_plugin(metaplugin)
-
-
-def on_popup_clone(widget, metaplugin):
-    player = com.get('neil.core.player')
-    pluginloader = metaplugin.get_pluginloader()
-    new_plugin = player.create_plugin(pluginloader)
-    print("on clone", pluginloader, new_plugin, player)
-    preset = Preset()
-    preset.pickup(metaplugin)
-    preset.apply(new_plugin)
-    player.history_commit("Clone plugin %s" % pluginloader.get_short_name())
-
-
-def on_popup_set_target(self, widget, metaplugin):
-    player = com.get('neil.core.player')
-    if player.autoconnect_target == metaplugin:
-        player.autoconnect_target = None
-    else:
-        player.autoconnect_target = metaplugin
-
-
-def on_popup_command(self, widget, metaplugin, subindex, index):
-    plugin.command((subindex << 8) | index)
-
-
-def on_popup_unmute_all(widget):
-    player = com.get('neil.core.player')
-    for metaplugin in reversed(list(player.get_plugin_list())):
-        info = common.get_plugin_infos().get(metaplugin)
+    for plugin in reversed(list(player.get_plugin_list())):
+        info = common.get_plugin_infos().get(plugin)
         info.muted = False
         metaplugin.set_mute(info.muted)
         info.reset_plugingfx()
 
 
-def on_machine_help(widget, metaplugin):
-    name = filenameify(metaplugin.get_pluginloader().get_name())
+def on_popup_solo(widget, plugin):
+    player = com.get('neil.core.player')
+    if player.solo_plugin != plugin:
+        player.solo(plugin)
+    else:
+        player.solo(None)
+
+
+def on_popup_bypass(widget, plugin):
+    com.get('neil.core.player').toggle_bypass(plugin)
+
+
+def on_popup_show_params(widget, plugin):
+    com.get('neil.core.parameterdialog.manager').show(plugin, widget)
+
+
+def on_popup_show_attribs(widget, plugin):
+    dlg = com.get('neil.core.attributesdialog', plugin, self)
+    dlg.run()
+    dlg.destroy()
+
+
+def on_popup_show_presets(widget, plugin):
+    com.get('neil.core.presetdialog.manager').show(plugin, widget)
+
+
+def on_popup_rename(widget, plugin):
+    text = gettext(widget, "Enter new plugin name:", prepstr(plugin.get_name()))
+    if text:
+        player = com.get('neil.core.player')
+        plugin.set_name(text)
+        player.history_commit("rename plugin")
+
+
+def on_popup_delete(widget, plugin):
+    com.get('neil.core.player').delete_plugin(plugin)
+
+
+def on_popup_delete_group(widget, plugins):
+    player = com.get('neil.core.player')
+    for plugin in plugins:
+        player.delete_plugin(plugin)
+
+
+
+def on_popup_set_target(self, widget, plugin):
+    player = com.get('neil.core.player')
+    if player.autoconnect_target == plugin:
+        player.autoconnect_target = None
+    else:
+        player.autoconnect_target = plugin
+
+
+def on_popup_command(self, widget, plugin, subindex, index):
+    plugin.command((subindex << 8) | index)
+
+
+def on_machine_help(widget, plugin):
+    name = filenameify(plugin.get_pluginloader().get_name())
     if not show_machine_manual(name):
         info = Gtk.MessageDialog(self.get_toplevel(), flags=0, type=Gtk.MessageType.INFO, buttons=Gtk.ButtonsType.OK, message_format="Sorry, there's no help for this plugin yet")
         info.run()
         info.destroy()
 
 
-def on_popup_disconnect(widget, metaplugin, index):
-    plugin = metaplugin.get_input_connection_plugin(index)
-    conntype = metaplugin.get_input_connection_type(index)
-    metaplugin.delete_input(plugin, conntype)
+
+def on_popup_disconnect(widget, plugin, index):
+    conn_plugin = plugin.get_input_connection_plugin(index)
+    conn_type = plugin.get_input_connection_type(index)
+    plugin.delete_input(conn_plugin, conn_type)
     player = com.get('neil.core.player')
     player.history_commit("disconnect")
 
 
-def on_popup_copy_chain(widget, plugins):
-    print("copy", plugins)
+# all the hard work for this is done in ccmwriter in ccm.cpp and is basically a c++ copy of clone_chain() hooked into the file save
+def on_popup_save_chain(widget, plugins):
+    get_plugin_chain(output_plugin, chain, player)
+
+
+# get the output connections for this plugin then call get_plugin_chains recursively with each of plugin in the output connections
+def get_plugin_chain(plugin, chain = {}, player = None):
+    if not player:
+        player = com.get('neil.core.player')
+
+    if type(plugin) == list:
+        for item in plugins:
+            add_plugin_chain(item, chain, player)
+
+        return chain.keys()
+
+    if is_root(plugin):
+        return chain.keys()
+
+    if plugin.get_id() not in chain.keys():
+        chain[plugin.get_id()] = None
+
+    for index in range(plugin.get_output_connection_count()):
+        output_id = plugin.get_output_connection_plugin(index).get_id()
+        output_plugin = player.get_plugin_by_id(output_id)
+        if output_id not in chain and not is_root(output_plugin):
+            chain[output_id] = None
+            output_plugin = player.get_plugin_by_id(output_id)
+            get_plugin_chain(output_plugin, chain, player)
+
+    return chain.keys()
+
+
+
+def clone_chain(src_plugin_list, offset = (0,0)):
+    player = com.get('neil.core.player')
+
+    plugins = {(plugin_id, None) for plugin_id in get_plugin_chain(src_plugin)}
+
+    for src_id in plugins.keys():
+        src_plugin = player.get_plugin_by_id(src_id)
+        if not is_root(src_plugin):
+            new_plugin = clone_plugin(player, src_plugin)
+            plugins[src_id] = new_plugin.get_id()
+
+    for (src_id, new_id) in plugins.items():
+        src_plugin = player.get_plugin_by_id(src_id)
+
+        new_plugin = player.get_plugin_by_id(new_id)
+        output_ids = [src_plugin.get_output_connection_plugin(index).get_id() for index in range(src_plugin.get_output_connection_count())]
+
+        for index, out_id in enumerate(output_ids):
+            out_id = plugins[out_id] if (out_id in plugins and plugins[out_id] != -1) else out_id
+            out_plugin = player.get_plugin_by_id(out_id)
+            conn_type = src_plugin.get_output_connection_type(index)
+            out_plugin.add_input(new_plugin, conn_type)
+
+    # patterns are copied after the making connections as the number of tracks in group 0 of the plugin patterns are the audio and event connections.
+    for src_id, new_id in plugins.items():
+        src_plugin = player.get_plugin_by_id(src_id)
+        new_plugin = player.get_plugin_by_id(new_id)
+
+        clone_plugin_patterns(src_plugin, new_plugin)
+        clone_preset(src_plugin, new_plugin)
+
+        x, y = src_plugin.get_position() + offset
+        new_plugin.set_position(x, y)
+
+    return plugins
+
+
+def on_popup_clone_chain(widget, src_plugin):
+    clone_chain([plugin], (0.1, 0.1))
+    com.get('neil.core.player').history_commit("Cloned plugin chain")
+
+
+def on_popup_clone_chains(widget, plugins, point = None):
+    def average_position(plugin_list):
+        add_positions = lambda posA, posB: (posA[0] + posB[0], posA[1] + posB[1])
+        plugin_positioms = [plugin.get_position() for plugin in plugin_list]
+        return reduce(add_positions, plugin_positions) / len(plugins)
+
+    if point is None:
+        offset = average_position(plugin_list) - point
+    else:
+        offset = (0.1, 0.1)
+
+    clone_chain(plugins, offset)
+
+    com.get('neil.core.player').history_commit("Cloned plugin chains")
+
+
+def on_popup_clone_plugin(widget, plugin):
+    player = com.get('neil.core.player')
+    new_plugin = clone_plugin(player, plugin)
+    clone_preset(plugin, new_plugin)
+    player.history_commit("Clone plugin %s" % new_plugin.get_pluginloader().get_short_name())
+
+
+def on_store_selection(widget, index, plugins):
+    com.get('neil.core.router.view').store_selection(index, plugins)
+
+
+def on_restore_selection(widget, index):
+    com.get('neil.core.router.view').restore_selection(index)
+
+
