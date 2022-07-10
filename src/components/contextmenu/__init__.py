7ee43b58af98e378326f20284924b166444a402c
Tim Hunt <tnhunt@gmail.com>
2022-07-10 14:44:29 +0100

    renamed the submenus

--- src/components/contextmenu/__init__.py
+++ src/components/contextmenu/__init__.py
@@ -4,10 +4,10 @@ such as plugins, patterns, and so on. based on the context object currently
 selected, items can choose to append themselves or not.
 """
 
-from .singleplugin import SinglePluginMenu
-from .multipleplugins import MultiplePluginMenu
-from .routermenu import RouterMenu
-from .connectionmenu import ConnectionMenu
+from .single_plugin_menu import SinglePluginMenu
+from .multiple_plugin_menu import MultiplePluginMenu
+from .router_menu import RouterMenu
+from .connection_menu import ConnectionMenu
 
 
 __neil__ = dict(
