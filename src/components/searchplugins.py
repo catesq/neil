2f094436bbfb41b814f3477264b78468fcf12be0
Tim Hunt <tnhunt@gmail.com>
2022-12-22 13:50:59 +0000

    restructure searchplugins

--- src/components/searchplugins.py
+++ src/components/searchplugins.py
@@ -20,22 +20,30 @@
 # Boston, MA  02110-1301, USA.
 
 from gi.repository import Gtk, Gdk, GdkPixbuf
-from gi.repository import GObject
-import neil.utils as utils, os, stat
-from neil.utils import new_stock_image_toggle_button, ObjectHandlerGroup
-from neil.utils import is_effect,is_generator,is_controller, is_root, is_other
-from neil.utils import prepstr, filepath, db2linear, linear2db,\
-     is_debug, filenameify, get_item_count, question, error,\
-     new_listview, add_scrollbars, get_clipboard_text,\
-     set_clipboard_text, gettext, new_stock_image_button,\
-     new_liststore, add_vscrollbar, get_adapter_name
-import neil.com as com
-import zzub
+
+
 from functools import cmp_to_key
-from neil.common import DRAG_TARGETS
 
+#                       is_debug, \
+from neil.utils import \
+                        is_root, is_generator, is_effect, is_controller, is_other, \
+                        prepstr, add_scrollbars, new_liststore, get_adapter_name
 
+from neil import com, common
 
+import zzub
+
+# the plugin search creates checkboxes for:
+    # plugin adapter: 'lv2', 'vst2', 'ladspa', 'dssi', 'zzub'
+    # machine type  : "generators", "effects", "controllers", "others"
+
+# toggling the checkbox will set/unset a config variable named:
+    # config.pluginlistbrowser_show_lv2
+    # config.pluginlistbrowser_show_vst2
+    # ... other plugin adapters ...
+    # config.pluginlistbrowser_show_generators
+    # config.pluginlistbrowser_show_effects
+    # ... other machine types ...
 
 class SearchPluginsDialog(Gtk.Window):
     __neil__ = dict(
@@ -44,105 +52,103 @@ class SearchPluginsDialog(Gtk.Window):
         categories = [
             'viewdialog',
             'view',
-            ]
-        )
+        ]
+     )
 
     __view__ = dict(
         label = "Search Plugins",
         order = 0,
+        shortcut = "<Shift>F3",
         toggle = True,
     )
 
     def __init__(self):
         Gtk.Window.__init__(self)
-        self.set_default_size(250, -1)
+        self.set_default_size(500, -1)
         self.vbox = Gtk.VBox()
         self.add(self.vbox)
         self.set_title("Search Plugins")
-        self.connect('delete-event', self.unrealize)
-        com.get("neil.core.icons") # make sure theme icons are loaded
-        self.searchterms = []
-        self.searchbox = Gtk.Entry()
-        self.treeview = Gtk.TreeView()
-        new_liststore(self.treeview, [
+        self.connect('delete-event', self.hide_popup_dialog)
+
+        com.get("neil.core.icons")        # make sure theme icons are loaded
+        cfg = com.get('neil.core.config') # get the config object with the 'pluginlistbrowser_show_' config settings
+
+        box_container = Gtk.HBox()
+        self.check_containers = [Gtk.VBox() for i in range(2)]
+
+        box_container.pack_start(self.check_containers[0], False, False, 0)
+        box_container.pack_end(self.check_containers[1], True, False, 0)
+        self.vbox.pack_end(box_container, False, False, 0)
+
+        self.machine_types = ["Generators", "Effects", "Controllers", "Others"]
+        self.machine_type_check = dict(zip(self.machine_types, [is_generator, is_effect, is_controller, is_other]))
+
+        labels = ['LV2', 'ladspa', 'dssi', 'zzub', 'VST 2']
+        urls = ['@zzub.org/dssidapter/', '@zzub.org/ladspadapter/', '@zzub.org/lv2adapter/', '@zzub.org/vstadapter/', '@zzub.org']
+
+        self.adapter_names = ['lv2', 'vst2', 'zzub', 'ladspa', 'dssi']
+        self.adapter_labels = dict(zip(self.adapter_names, labels))
+        self.adapter_urls = dict(zip(self.adapter_names, urls))
+
+        # prepare the plugin list
+        self.treeview = Gtk.TreeView() # the treeview has the list of matching plugins
+        self.searchbox = Gtk.Entry()   # the text entry for the plugin list
+        self.searchterms = []          # the search box entry split into words
+
+        # the store is a gtk.liststore and will contain every plugin. it is filtered so only matching plugins are shown in the treeview
+        (liststore, column_controls) = new_liststore(self.treeview, [
             ('Icon', GdkPixbuf.Pixbuf),
             ('Name', str, dict(markup=True)),
             (None, object),
             (None, str, dict(markup=True)),
         ])
 
-        # checkboxes
-        box_container = Gtk.HBox()
-        self.check_containers = [Gtk.VBox() for i in range(2)]
-        
-        box_container.pack_start(self.check_containers[0], False, False, 0)
-        box_container.pack_end(self.check_containers[1], True, False, 0)
-        self.vbox.pack_end(box_container, False, False, 0)
+        self.populate(liststore) # add all the plugins to the liststore
 
-        self.show_generators_button = Gtk.CheckButton(label="Generators")
-        # self.show_generators_button.set_halign()
-        self.check_containers[0].add(self.show_generators_button)
-        self.show_generators_button.connect("toggled", self.on_checkbox_changed)
+        # build the filter for the plugin list. the list is filtered in self.filter_item()
+        self.filter = liststore.filter_new()
+        self.treeview.set_model(self.filter)
+        self.filter.set_visible_func(self.filter_item, data=None)
 
-        self.show_effects_button = Gtk.CheckButton(label="Effects")
-        self.check_containers[0].add(self.show_effects_button)
-        self.show_effects_button.connect("toggled", self.on_checkbox_changed)
+        # build checkboxes for all machine types and plugin adapters
+        def build_checkbox(container, name):
+            checkbox = Gtk.CheckButton(label=name)
+            container.add(checkbox)
+            checkbox.connect("toggled", self.on_checkbox_changed)
+            return checkbox
 
-        self.show_controllers_button = Gtk.CheckButton(label="Controllers")
-        self.check_containers[0].add(self.show_controllers_button)
-        self.show_controllers_button.connect("toggled", self.on_checkbox_changed)
+        self.checkboxes = {}
+        for machine_type in self.machine_types:
+            self.checkboxes[machine_type] = build_checkbox(self.check_containers[0], machine_type)
 
-        
-        
-        self.show_aswell_or_only_switch = Gtk.Switch()
-        self.show_aswell_or_only_switch.connect("notify::active", self.on_switch_activated)
-        show_box = Gtk.HBox()
-        show_box.add(Gtk.Label("Show"))
-        show_box.add(self.show_aswell_or_only_switch)
-        self.check_containers[1].add(show_box)
+        for adapter_name in self.adapter_names:
+            self.checkboxes[adapter_name] = build_checkbox(self.check_containers[1], self.adapter_labels[adapter_name])
 
-        self.show_lv2_button = Gtk.CheckButton(label="LV2")
-        self.check_containers[1].add(self.show_lv2_button)
-        self.show_lv2_button.connect("toggled", self.on_checkbox_changed)
+        # read current config settings to check/uncheck each checkbox
+        for plugin_name in self.checkboxes.keys():
+            attr_name = 'pluginlistbrowser_show_' + plugin_name.lower()
+            config_value = getattr(cfg, attr_name)
+            self.checkboxes[plugin_name].set_active(config_value)
 
-        self.show_vst2_button = Gtk.CheckButton(label="VST2")
-        self.check_containers[1].add(self.show_vst2_button)
-        self.show_vst2_button.connect("toggled", self.on_checkbox_changed)
-
-        self.show_ladspa_button = Gtk.CheckButton(label="Ladspa")
-        self.check_containers[1].add(self.show_ladspa_button)
-        self.show_ladspa_button.connect("toggled", self.on_checkbox_changed)
-
-        self.show_dssi_button = Gtk.CheckButton(label="Dssi")
-        self.check_containers[1].add(self.show_dssi_button)
-        self.show_dssi_button.connect("toggled", self.on_checkbox_changed)
-
-        self.store = self.treeview.get_model()
         self.treeview.set_headers_visible(False)
         self.treeview.set_rules_hint(True)
-        self.populate()
+        self.treeview.set_tooltip_column(3)
+        self.treeview.drag_source_set( Gdk.ModifierType.BUTTON1_MASK | Gdk.ModifierType.BUTTON3_MASK, common.DRAG_TARGETS, Gdk.DragAction.COPY )
+
         scrollbars = add_scrollbars(self.treeview)
         self.vbox.pack_start(scrollbars, True, True, 0)
         self.vbox.pack_end(self.searchbox, False, False, 0)
+
         self.searchbox.connect("changed", self.on_entry_changed)
-        self.filter = self.store.filter_new()
-        self.filter.set_visible_func(self.filter_item, data=None)
-        self.treeview.set_model(self.filter)
-        self.treeview.set_tooltip_column(3)
-        self.treeview.drag_source_set( Gdk.ModifierType.BUTTON1_MASK | Gdk.ModifierType.BUTTON3_MASK, DRAG_TARGETS, Gdk.DragAction.COPY )
-        
-        cfg = com.get('neil.core.config')
         self.searchbox.set_text(cfg.pluginlistbrowser_search_term)
-        self.show_generators_button.set_active(cfg.pluginlistbrowser_show_generators)
-        self.show_effects_button.set_active(cfg.pluginlistbrowser_show_effects)
-        self.show_controllers_button.set_active(cfg.pluginlistbrowser_show_controllers)
-        self.show_aswell_or_only_switch.set_active(cfg.pluginlistbrowser_show_aswell_or_only)
-        self.show_lv2_button.set_active(cfg.pluginlistbrowser_show_lv2)
-        self.show_vst2_button.set_active(cfg.pluginlistbrowser_show_vst2)
-        self.show_ladspa_button.set_active(cfg.pluginlistbrowser_show_ladspa)
+
         self.set_size_request(400, 600)
         self.connect('realize', self.realize)
         self.conn_id = False
+
+    def hide_popup_dialog(self, widget, data):
+        self.hide()
+        return True
 
     def realize(self, widget):
         if not self.conn_id:
@@ -154,16 +160,10 @@ class SearchPluginsDialog(Gtk.Window):
 
     def get_icon_name(self, pluginloader):
         uri = pluginloader.get_uri()
-        if uri.startswith('@zzub.org/dssidapter/'):
-            return 'dssi'
-        if uri.startswith('@zzub.org/ladspadapter/'):
-            return 'ladspa'
-        if uri.startswith('@psycle.sourceforge.net/'):
-            return 'psycle' 
-        if uri.startswith('@zzub.org/lv2adapter/'):
-            return 'lv2'
-        if uri.startswith('@zzub.org/vstadapter/'):
-            return 'vst2'
+        for (adapter_name, url_prefix) in self.adapter_urls.items():
+            if uri.startswith(url_prefix):
+                return adapter_name
+
         filename = pluginloader.get_name()
         filename = filename.strip().lower()
         for c in '():[]/,.!"\'$%&\\=?*#~+-<>`@ ':
@@ -182,101 +182,66 @@ class SearchPluginsDialog(Gtk.Window):
             selection_data.set(target, 8, bytes(uri.encode("utf-8")))
 
     def on_entry_changed(self, widget):
-        text = self.searchbox.get_text()
         cfg = com.get('neil.core.config')
+
+        text = self.searchbox.get_text()
         cfg.pluginlistbrowser_search_term = text
-        terms = [word.strip().split(' ') for word in text.lower().strip().split(',')]
+        terms = [word.strip() for word in text.lower().replace(',', '').split(' ')]
+
         self.searchterms = terms
         self.filter.refilter()
 
-    def on_checkbox_changed(self, check):
-        active = check.get_active()
+    def on_checkbox_changed(self, checkbox):
         cfg = com.get('neil.core.config')
-        lbl = check.get_label()
-        if lbl == "Generators":
-            cfg.pluginlistbrowser_show_generators = active
-        elif lbl == "Effects":
-            cfg.pluginlistbrowser_show_effects = active
-        elif lbl == "Controllers":
-            cfg.pluginlistbrowser_show_controllers = active
-        elif lbl == "LV2":
-            cfg.pluginlistbrowser_show_lv2 = active
-        elif lbl == "VST2":
-            cfg.pluginlistbrowser_show_vst2 = active
-        elif lbl == "Ladspa":
-            cfg.pluginlistbrowser_show_ladpsa = active
-        elif lbl == "Dssi":
-            cfg.pluginlistbrowser_show_dssi = active
+        label = checkbox.get_label().replace(' ', '').lower()
+        setattr(cfg, 'pluginlistbrowser_show_' + label, checkbox.get_active())
         self.filter.refilter()
 
-    def on_switch_activated(self, switch, other):
-        cfg = com.get('neil.core.config')
-        cfg.pluginlistbrowser_show_aswell_or_only = switch.get_active()
-        self.filter.refilter()
+    def match_machine_type(self, pluginloader):
+        for machine_type in self.machine_types:
+            # check_type_func is one of:  is_generator  is_controller  is_effect  is_other
+            check_type_func = self.machine_type_check[machine_type]
+            is_checkbox_active = self.checkboxes[machine_type].get_active()
+            if is_checkbox_active and check_type_func(pluginloader):
+                return True
+        return False
 
+    def is_active_adapter(self, pluginloader):
+        # adapter name one of:  lv2  vst2  zzub  ladspa  dssi
+        adapter_name = get_adapter_name(pluginloader)
+        if adapter_name in self.adapter_names:
+            return self.checkboxes[adapter_name].get_active()
+        return False
 
+    #
     def filter_item(self, model, it, data):
         pluginloader = model.get(it, 2)[0]
-#        if get_adapter_name(pluginloader) == "vst2":
-#        print("SEARCH", pluginloader.get_loader_name(), "is gen", is_generator(pluginloader), is_effect(pluginloader), is_controller(pluginloader))
 
-        if is_other(pluginloader):
-            return False 
-
-        if not self.show_generators_button.get_active() and is_generator(pluginloader):
-            return False
-        if not self.show_effects_button.get_active() and is_effect(pluginloader):
-            return False
-        if not self.show_controllers_button.get_active() and is_controller(pluginloader):
-            return False
-
-        adapter_name = get_adapter_name(pluginloader)
-
-
-        if adapter_name == "lv2" and not self.show_lv2_button.get_active():
-            return False
-        elif adapter_name == "ladspa" and not self.show_ladspa_button.get_active():
-            return False
-        elif adapter_name == "dssi" and not self.show_dssi_button.get_active():
-            return False
-        elif adapter_name == "zzub" and self.show_aswell_or_only_switch.get_active():
-            return False
-        elif adapter_name == "vst2" and not self.show_vst2_button.get_active():
+        if not self.match_machine_type(pluginloader) or not self.is_active_adapter(pluginloader):
             return False
 
         name = pluginloader.get_name().lower()
         if len(self.searchterms) > 0:
+            unmatched = False
             for group in self.searchterms:
                 for word in group:
                     if word not in name:
-                        return False
-                    
-            # if found:
-                # print(name, "matched", self.searchterms)
-                # return True
-            # return False
+                        unmatched = True
+            return not unmatched
+
         return True
 
-    def populate(self):
-        player = com.get('neil.core.player')
-        plugins = {}
+    def populate(self, liststore):
         cfg = com.get('neil.core.config')
-        for pluginloader in player.get_pluginloader_list():
-            plugins[pluginloader.get_uri()] = pluginloader
         theme = Gtk.IconTheme.get_default()
+
         def get_type_rating(n):
             uri = n.get_uri()
-            if uri.startswith('@zzub.org/dssidapter/'):
-                return 1
-            if uri.startswith('@zzub.org/ladspadapter/'):
-                return 1
-            if uri.startswith('@psycle.sourceforge.net/'):
-                return 2
-            if uri.startswith('@zzub.org/lv2adapter/'):
-                return 3
-            if uri.startswith('@zzub.org/vstadapter/'):
-                return 4
+            for (adapter_name, url_prefix) in self.adapter_urls.items():
+                if uri.startswith(url_prefix):
+                    return list(self.adapter_urls.values()).index(url_prefix) + 1
             return 0
+
         def get_icon_rating(n):
             icon = self.get_icon_name(n)
             if icon and theme.has_icon(icon):
@@ -292,8 +257,10 @@ class SearchPluginsDialog(Gtk.Window):
                 return 2
             else:
                 return 3
+
         def cmp(a, b):
             return int(a > b) - int(a < b)
+
         def cmp_child(a,b):
             c = cmp(get_type_rating(a), get_type_rating(b))
             if c != 0:
@@ -305,6 +272,7 @@ class SearchPluginsDialog(Gtk.Window):
             if c != 0:
                 return c
             return cmp(a.get_name().lower(),b.get_name().lower())
+
         def get_type_text(pl):
             if is_generator(pl):
                 return '<span color="' + cfg.get_color('MV Generator') + '">Generator</span>'
@@ -312,10 +280,14 @@ class SearchPluginsDialog(Gtk.Window):
                 return '<span color="' + cfg.get_color('MV Effect') + '">Effect</span>'
             elif is_controller(pl):
                 return '<span color="' + cfg.get_color('MV Controller') + '">Controller</span>'
-            elif is_root(pl):
-                return "Root"
             else:
-                return "Other"
+                return '<span color="' + cfg.get_color('MV Other') + '">Other</span>'
+
+        plugins = {}
+        player = com.get('neil.core.player')
+        for pluginloader in player.get_pluginloader_list():
+            plugins[pluginloader.get_uri()] = pluginloader
+
         for pl in sorted(plugins.values(), key=cmp_to_key(cmp_child)):
             name = prepstr(pl.get_name())
             text = '<b>' + name + '</b>\n<small>' + get_type_text(pl) + '</small>'
@@ -328,10 +300,10 @@ class SearchPluginsDialog(Gtk.Window):
             acount = pl.get_attribute_count()
             tooltip += '%i global parameters, %i track parameters, %i attributes\n\n' % (gpcount, tpcount, acount)
             author = pl.get_author().replace('<', '&lt;').replace('>', '&gt;')
-            tooltip += 'Written by ' + prepstr(author)
+            tooltip += 'Written by   ' + prepstr(author)
             if icon and theme.has_icon(icon):
                 pixbuf = theme.load_icon(icon, Gtk.IconSize.MENU, 0)
-            self.store.append([pixbuf, text, pl, tooltip])
+            liststore.append([pixbuf, text, pl, tooltip])
 
 
 __all__ = [
@@ -341,7 +313,7 @@ __all__ = [
 __neil__ = dict(
     classes = [
         SearchPluginsDialog,
-        ],
+    ],
 )
 
 if __name__ == '__main__':
