e7d6f16375562a01263aa04b7c33ce73e858647b
Tim Hunt <tnhunt@gmail.com>
2023-02-13 15:02:17 +0000

    add clear/clone pattern for when multiple rows selected

--- src/components/sequencer/pattern_list.py
+++ src/components/sequencer/pattern_list.py
@@ -1,10 +1,22 @@
-
 import gi
 from gi.repository import Gtk
 from neil.com import com
 
 
-# the on_ function are actions for the context menu of the pattern list 
+# a couple of helper function for the context menu
+
+# clear one of the squencer patterns
+def clear_pattern(plugin, pattern_index):
+    length = plugin.get_pattern_length(pattern_index)
+    name = plugin.get_pattern_name(pattern_index)
+    new_pattern = plugin.create_pattern(length)
+    new_pattern.set_name(name)
+    plugin.update_pattern(pattern_index, new_pattern)
+
+
+
+
+# these are actions for the context menu of the pattern list when a single pattern is selected 
 def on_create(item, treeview, _):
     treeview.sequencer_panel.treeview_create_pattern(treeview)
 
@@ -15,17 +27,15 @@ def on_clone(item, treeview, pattern_index):
 
 def on_rename(item, treeview, pattern_index):
     treeview.sequencer_panel.treeview_rename_pattern(treeview, pattern_index)
-    
+
 
 def on_clear(item, treeview, pattern_index):
     plugin = treeview.sequencer_panel.get_active_plugin()
-    length = plugin.get_pattern_length(pattern_index)
-    name = plugin.get_pattern_name(pattern_index)
-    new_pattern = plugin.create_pattern(length)
-    new_pattern.set_name(name)
-    plugin.update_pattern(pattern_index, new_pattern)
+    clear_pattern(plugin, pattern_index)
+
     player = com.get('neil.core.player')
     player.history_commit("clear pattern")
+
 
 
 def on_delete(item, treeview, pattern_index):
@@ -34,12 +44,30 @@ def on_delete(item, treeview, pattern_index):
         player = com.get('neil.core.player')
         player.history_commit("remove pattern")
 
+
+# these are the actions for when a group of items are selected
+def on_clear_list(item, treeview, pattern_indexes):
+    plugin = treeview.sequencer_panel.get_active_plugin()
+    for index in pattern_indexes:
+        clear_pattern(plugin, index)
+
+    player = com.get('neil.core.player')
+    player.history_commit("clear patterns")
+
+
+def on_clone_list(item, treeview, pattern_indexes):
+    for pattern_index in pattern_indexes:
+        treeview.sequencer_panel.treeview_clone_pattern(treeview, pattern_index)
+
+
 def on_delete_list(item, treeview, pattern_indexes):
     for pattern_index in pattern_indexes:
         treeview.sequencer_panel.get_active_plugin().remove_pattern(pattern_index)
 
     player = com.get('neil.core.player')
     player.history_commit("remove patterns")
+
+
 
 
 # the pattern list. embedded on left of the sequencer panel defined in panel.py 
@@ -94,47 +122,70 @@ class SequencerPatternListTreeView(Gtk.TreeView):
         item.connect('activate', callback, self, callback_data)
         item.show()
 
-    def build_menu_for_selected_rows(self, menu):
+
+    def build_menu_for_multiple_rows(self, menu, selected_rows):
+        menu_entries = [
+            ("Clone patterns", on_clone_list, selected_rows),
+            ("Clear patterns", on_clear_list, selected_rows),
+            ("Delete patterns", on_delete_list, selected_rows),
+        ]
+
+        for label, callback, callback_data in menu_entries:
+            self.add_menu_item(menu, label, callback, callback_data)
+
+
+    def build_menu_for_single_row(self, menu, selected_index):
+        # the "-2" is because the first two items in the pattern list are not real patterns, ie not found with plugin.get_pattern(selected_index)
+        # they are the pattern list entries for auto generated mute and silence patterns 
+        menu_entries = [
+            ("Clone pattern", on_clone, selected_index - 2),
+            ("Pattern properties", on_rename, selected_index - 2),
+            ("Clear pattern", on_clear, selected_index - 2),
+            ("Delete pattern", on_delete, selected_index - 2),
+        ]
+
+        for label, callback, callback_data in menu_entries:
+            self.add_menu_item(menu, label, callback, callback_data)
+
+
+    # get the selected rows in the pattern list
+    #   but if they're selected then remove the first two rows, 
+    #   they are not real patterns, they're the mute and silence patterns which can't be cleared, deleted or copied.
+    def get_viable_selected_rows(self):
         (model, paths) = self.get_selection().get_selected_rows()
-        pattern_nums = [path[0] - 2 for path in paths]
+        return [path[0] - 2 for path in paths if path[0] > 1]
 
-        menu_entries = [
-            ("Delete patterns", on_delete_list, pattern_nums),
-        ]
-
-        for label, callback, callback_data in menu_entries:
-            self.add_menu_item(menu, label, callback, callback_data)
-
-
-    # when no patterns are selected then
-    def build_menu_for_row_at_clickpos(self, menu, path):
-        print("clickpos path", path[0][0] - 2)
-        menu_entries = [
-            ("Clone pattern", on_clone, path[0][0] - 2),
-            ("Pattern properties", on_rename, path[0][0] - 2),
-            ("Clear pattern", on_clear, path[0][0] - 2),
-            ("Delete pattern", on_delete, path[0][0] - 2),
-        ]
-
-        for label, callback, callback_data in menu_entries:
-            self.add_menu_item(menu, label, callback, callback_data)
 
     def on_pattern_list_button(self, treeview, event):
+        # only show context menu on right clicks when an plugin in the sequencer panel is active is highlighted
         if not self.sequencer_panel.get_active_plugin() or not event.button == 3:
             return False
 
         menu = Gtk.Menu()
 
+        # show different menu depending on whether any actual rows in the pattern list are selected
         click_path = self.get_path_at_pos(int(event.x), int(event.y))
-        selected_count = self.get_selection().count_selected_rows()
+        selected_rows = self.get_viable_selected_rows()
 
         self.add_menu_item(menu, "New pattern", on_create, None)
 
-        if selected_count > 0:
-            self.build_menu_for_selected_rows(menu)
-        elif click_path != None:
-            self.build_menu_for_row_at_clickpos(menu, click_path)
+        # want the row highlight to appear when right clicking items
+        suppress_event = True
+
+        # the ordering of these tests is deliberately odd
+        # the handling was for len(selected_rows) > 1 was originally the second if statement  
+        # but this meant: if row 2 was highlighted but row 4 was right clicked then the menu would popup over row 2. the popup menu over row 4 felt better
+        # so rearranged the tests so right click over a row happens earlier
+        if len(selected_rows) > 1:
+            self.build_menu_for_multiple_rows(menu, selected_rows)
+        elif click_path != None and click_path[0][0] >= 2:
+            self.build_menu_for_single_row(menu, click_path[0][0])
+            suppress_event = False
+        elif len(selected_rows) == 1:
+            self.build_menu_for_single_row(menu, selected_rows[0])
+
 
         menu.popup(None, None, None, None, event.button, event.time)
 
-        return True
+        return suppress_event
+        
