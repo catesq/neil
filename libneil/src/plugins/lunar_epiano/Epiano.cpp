deb2793d30d571ba43067c8363014bc63e99520b
Tim Hunt <tnhunt@gmail.com>
2024-03-17 12:31:30 +0000

    fixed indentation

--- libneil/src/plugins/lunar_epiano/Epiano.cpp
+++ libneil/src/plugins/lunar_epiano/Epiano.cpp
@@ -3,27 +3,24 @@
 
 #include "Epiano.hpp"
 
-LunarEpiano::LunarEpiano() 
-{
+LunarEpiano::LunarEpiano() {
   global_values = &gval;
   track_values = &tval;
   attributes = 0;
   track_count = 1;
 }
 
-LunarEpiano::~LunarEpiano() 
-{
+LunarEpiano::~LunarEpiano() {
 
 }
 
-void LunarEpiano::init(zzub::archive* pi) 
-{
+void LunarEpiano::init(zzub::archive* pi) {
   waves = epianoData;
   //Waveform data and keymapping
-  kgrp[ 0].root = 36; kgrp[ 0].high = 39; //C1
-  kgrp[ 3].root = 43; kgrp[ 3].high = 45; //G1
-  kgrp[ 6].root = 48; kgrp[ 6].high = 51; //C2
-  kgrp[ 9].root = 55; kgrp[ 9].high = 57; //G2
+  kgrp[0].root = 36; kgrp[0].high = 39; //C1
+  kgrp[3].root = 43; kgrp[3].high = 45; //G1
+  kgrp[6].root = 48; kgrp[6].high = 51; //C2
+  kgrp[9].root = 55; kgrp[9].high = 57; //G2
   kgrp[12].root = 60; kgrp[12].high = 63; //C3
   kgrp[15].root = 67; kgrp[15].high = 69; //G3
   kgrp[18].root = 72; kgrp[18].high = 75; //C4
@@ -31,41 +28,41 @@ void LunarEpiano::init(zzub::archive* pi)
   kgrp[24].root = 84; kgrp[24].high = 87; //C5
   kgrp[27].root = 91; kgrp[27].high = 93; //G5
   kgrp[30].root = 96; kgrp[30].high = 999; //C6
-		
-  kgrp[0].pos = 0; kgrp[0].end = 8476; kgrp[0].loop = 4400;  
-  kgrp[1].pos = 8477; kgrp[1].end = 16248; kgrp[1].loop = 4903;  
-  kgrp[2].pos = 16249; kgrp[2].end = 34565; kgrp[2].loop = 6398;  
-  kgrp[3].pos = 34566; kgrp[3].end = 41384; kgrp[3].loop = 3938;  
+
+  kgrp[0].pos = 0; kgrp[0].end = 8476; kgrp[0].loop = 4400;
+  kgrp[1].pos = 8477; kgrp[1].end = 16248; kgrp[1].loop = 4903;
+  kgrp[2].pos = 16249; kgrp[2].end = 34565; kgrp[2].loop = 6398;
+  kgrp[3].pos = 34566; kgrp[3].end = 41384; kgrp[3].loop = 3938;
   kgrp[4].pos = 41385; kgrp[4].end = 45760; kgrp[4].loop = 1633; //was 1636;  
-  kgrp[5].pos = 45761; kgrp[5].end = 65211; kgrp[5].loop = 5245;  
-  kgrp[6].pos = 65212; kgrp[6].end = 72897; kgrp[6].loop = 2937;  
+  kgrp[5].pos = 45761; kgrp[5].end = 65211; kgrp[5].loop = 5245;
+  kgrp[6].pos = 65212; kgrp[6].end = 72897; kgrp[6].loop = 2937;
   kgrp[7].pos = 72898; kgrp[7].end = 78626; kgrp[7].loop = 2203; //was 2204;  
-  kgrp[8].pos = 78627; kgrp[8].end = 100387; kgrp[8].loop = 6368;  
-  kgrp[9].pos = 100388; kgrp[9].end = 116297; kgrp[9].loop = 10452;  
+  kgrp[8].pos = 78627; kgrp[8].end = 100387; kgrp[8].loop = 6368;
+  kgrp[9].pos = 100388; kgrp[9].end = 116297; kgrp[9].loop = 10452;
   kgrp[10].pos = 116298; kgrp[10].end = 127661; kgrp[10].loop = 5217; //was 5220; 
-  kgrp[11].pos = 127662; kgrp[11].end = 144113; kgrp[11].loop = 3099;  
-  kgrp[12].pos = 144114; kgrp[12].end = 152863; kgrp[12].loop = 4284;  
-  kgrp[13].pos = 152864; kgrp[13].end = 173107; kgrp[13].loop = 3916;  
-  kgrp[14].pos = 173108; kgrp[14].end = 192734; kgrp[14].loop = 2937;  
-  kgrp[15].pos = 192735; kgrp[15].end = 204598; kgrp[15].loop = 4732;  
-  kgrp[16].pos = 204599; kgrp[16].end = 218995; kgrp[16].loop = 4733;  
-  kgrp[17].pos = 218996; kgrp[17].end = 233801; kgrp[17].loop = 2285;  
-  kgrp[18].pos = 233802; kgrp[18].end = 248011; kgrp[18].loop = 4098;  
-  kgrp[19].pos = 248012; kgrp[19].end = 265287; kgrp[19].loop = 4099;  
-  kgrp[20].pos = 265288; kgrp[20].end = 282255; kgrp[20].loop = 3609;  
-  kgrp[21].pos = 282256; kgrp[21].end = 293776; kgrp[21].loop = 2446;  
-  kgrp[22].pos = 293777; kgrp[22].end = 312566; kgrp[22].loop = 6278;  
-  kgrp[23].pos = 312567; kgrp[23].end = 330200; kgrp[23].loop = 2283;  
-  kgrp[24].pos = 330201; kgrp[24].end = 348889; kgrp[24].loop = 2689;  
-  kgrp[25].pos = 348890; kgrp[25].end = 365675; kgrp[25].loop = 4370;  
-  kgrp[26].pos = 365676; kgrp[26].end = 383661; kgrp[26].loop = 5225;  
-  kgrp[27].pos = 383662; kgrp[27].end = 393372; kgrp[27].loop = 2811;  
+  kgrp[11].pos = 127662; kgrp[11].end = 144113; kgrp[11].loop = 3099;
+  kgrp[12].pos = 144114; kgrp[12].end = 152863; kgrp[12].loop = 4284;
+  kgrp[13].pos = 152864; kgrp[13].end = 173107; kgrp[13].loop = 3916;
+  kgrp[14].pos = 173108; kgrp[14].end = 192734; kgrp[14].loop = 2937;
+  kgrp[15].pos = 192735; kgrp[15].end = 204598; kgrp[15].loop = 4732;
+  kgrp[16].pos = 204599; kgrp[16].end = 218995; kgrp[16].loop = 4733;
+  kgrp[17].pos = 218996; kgrp[17].end = 233801; kgrp[17].loop = 2285;
+  kgrp[18].pos = 233802; kgrp[18].end = 248011; kgrp[18].loop = 4098;
+  kgrp[19].pos = 248012; kgrp[19].end = 265287; kgrp[19].loop = 4099;
+  kgrp[20].pos = 265288; kgrp[20].end = 282255; kgrp[20].loop = 3609;
+  kgrp[21].pos = 282256; kgrp[21].end = 293776; kgrp[21].loop = 2446;
+  kgrp[22].pos = 293777; kgrp[22].end = 312566; kgrp[22].loop = 6278;
+  kgrp[23].pos = 312567; kgrp[23].end = 330200; kgrp[23].loop = 2283;
+  kgrp[24].pos = 330201; kgrp[24].end = 348889; kgrp[24].loop = 2689;
+  kgrp[25].pos = 348890; kgrp[25].end = 365675; kgrp[25].loop = 4370;
+  kgrp[26].pos = 365676; kgrp[26].end = 383661; kgrp[26].loop = 5225;
+  kgrp[27].pos = 383662; kgrp[27].end = 393372; kgrp[27].loop = 2811;
   kgrp[28].pos = 383662; kgrp[28].end = 393372; kgrp[28].loop = 2811; //ghost
-  kgrp[29].pos = 393373; kgrp[29].end = 406045; kgrp[29].loop = 4522;  
-  kgrp[30].pos = 406046; kgrp[30].end = 414486; kgrp[30].loop = 2306;  
+  kgrp[29].pos = 393373; kgrp[29].end = 406045; kgrp[29].loop = 4522;
+  kgrp[30].pos = 406046; kgrp[30].end = 414486; kgrp[30].loop = 2306;
   kgrp[31].pos = 406046; kgrp[31].end = 414486; kgrp[31].loop = 2306; //ghost
-  kgrp[32].pos = 414487; kgrp[32].end = 422408; kgrp[32].loop = 2169;  
-		
+  kgrp[32].pos = 414487; kgrp[32].end = 422408; kgrp[32].loop = 2169;
+
   //extra xfade looping...
   for (long k = 0; k < 28; k++) {
     long p0 = kgrp[k].end;
@@ -79,7 +76,7 @@ void LunarEpiano::init(zzub::archive* pi)
       xf += dxf;
     }
   }
-		
+
   //initialise...
   for (long v = 0; v < NVOICES; v++) {
     voice[v].note = 0;
@@ -92,29 +89,29 @@ void LunarEpiano::init(zzub::archive* pi)
   sustain = 0;
   tl = tr = lfo0 = dlfo = 0.0f;
   lfo1 = 1.0f;
-      
+
   sizevel = 0;	// this is uninitialized in the original epiano (?)
   //update();
-	
+
   Fs = _master_info->samples_per_second;
   iFs = 1.0f / Fs;
   dlfo = 6.283f * iFs * (float)exp(6.22f * param[5] - 2.61f); //lfo rate 
 }
 
-void LunarEpiano::update()
-{
-  size = (long)(12.0f * param[2] - 6.0f);  
+void LunarEpiano::update() {
+  size = (long)(12.0f * param[2] - 6.0f);
   treb = 4.0f * param[3] * param[3] - 1.0f; //treble gain
   if (param[3] > 0.5f) {
-    tfrq = 14000.0f; 
-  } else { 
+    tfrq = 14000.0f;
+  }
+  else {
     tfrq = 5000.0f;
   }
   tfrq = 1.0f - (float)exp(-iFs * tfrq);
   rmod = lmod = param[4] + param[4] - 1.0f; //lfo depth
   if (param[4] < 0.5f) {
     rmod = -rmod;
-  }  
+  }
   dlfo = 6.283f * iFs * (float)exp(6.22f * param[5] - 2.61f); //lfo rate
   velsens = 1.0f + param[6] + param[6];
   if (param[6] < 0.25f) {
@@ -128,20 +125,19 @@ void LunarEpiano::update()
   overdrive = 1.8f * param[11];
 }
 
-void LunarEpiano::noteOn(long note, long velocity, long vl)
-{
+void LunarEpiano::noteOn(long note, long velocity, long vl) {
   float l = 99.0f;
-  long k, s; 
+  long k, s;
   if (velocity > 0) {
     voice[vl].f0 = voice[vl].f1 = 0.0f;	// since we're not doing polyphony, this is the remainder of the voice-picking code
     k = (note - 60) * (note - 60);
     l = fine + random * ((float)(k % 13) - 6.5f);  //random & fine tune
-    if (note > 60) { 
+    if (note > 60) {
       l += stretch * (float)k; //stretch
     }
     s = size;
-    if (velocity > 40) { 
-      s += (long)(sizevel * (float)(velocity - 40));  
+    if (velocity > 40) {
+      s += (long)(sizevel * (float)(velocity - 40));
     }
     k = 0;
     while (note > (kgrp[k].high + s)) {
@@ -173,7 +169,7 @@ void LunarEpiano::noteOn(long note, long velocity, long vl)
     }
     voice[vl].ff = l * l * iFs;
     voice[vl].note = note; //note->pan
-    if (note <  12) { 
+    if (note < 12) {
       note = 12;
     }
     if (note > 108) {
@@ -186,25 +182,25 @@ void LunarEpiano::noteOn(long note, long velocity, long vl)
       note = 44; //limit max decay length
     }
     voice[vl].dec = (float)exp(-iFs * exp(-1.0 + 0.03 * (double)note - 2.0f * param[0]));
-  } else {   
+  }
+  else {
     if (sustain == 0) {
       voice[vl].dec = (float)exp(-iFs * exp(6.0 + 0.01 * (double)note - 5.0 * param[1]));
-    } else { 
-      voice[vl].note = SUSTAIN; 
+    }
+    else {
+      voice[vl].note = SUSTAIN;
     }
   }
-} 
+}
 
-void LunarEpiano::check_parameter(int p, int i, bool& changed) 
-{
+void LunarEpiano::check_parameter(int p, int i, bool& changed) {
   if (i != 0xffff) {
     param[p] = i * 0.001f;
     changed = true;
   }
 }
 
-void LunarEpiano::destroy() 
-{
+void LunarEpiano::destroy() {
 
 }
 
@@ -212,13 +208,11 @@ void LunarEpiano::stop() {
 
 }
 
-void LunarEpiano::set_track_count(int tracks) 
-{
+void LunarEpiano::set_track_count(int tracks) {
   track_count = tracks;
 }
 
-void LunarEpiano::process_events() 
-{
+void LunarEpiano::process_events() {
   bool needs_update = false;
   check_parameter(0, gval.envdecay, needs_update);
   check_parameter(1, gval.envrelease, needs_update);
@@ -236,6 +230,7 @@ void LunarEpiano::process_events()
     update();
   }
   int event = 0;
+
   for (unsigned int i = 0; i < track_count; i++) {
     long velocity = 127;
     if (tval[i].volume != 0xff) {
@@ -245,20 +240,20 @@ void LunarEpiano::process_events()
       int note = 12 * (tval[i].note >> 4) + (tval[i].note & 0xf) - 1;
       notes[event++] = 1;
       if (tval[i].note == zzub::note_value_off) {
-	notes[event++] = voice[i].note;
-	notes[event++] = 0;
-      } else {
-	notes[event++] = note;
-	notes[event++] = velocity;
+        notes[event++] = voice[i].note;
+        notes[event++] = 0;
+      }
+      else {
+        notes[event++] = note;
+        notes[event++] = velocity;
       }
       notes[event++] = i;
-    }				
+    }
   }
   notes[event] = EVENTS_DONE;
 }
 
-bool LunarEpiano::process_stereo(float **pin, float **pout, int n, int mode) 
-{ 
+bool LunarEpiano::process_stereo(float** pin, float** pout, int n, int mode) {
   float* out0 = pout[0];
   float* out1 = pout[1];
   long event = 0, frame = 0, frames;
@@ -272,39 +267,39 @@ bool LunarEpiano::process_stereo(float **pin, float **pout, int n, int mode)
       frames = n;
     }
     frames -= frame;
-    frame += frames;		
+    frame += frames;
     while (--frames >= 0) {
-      VOICE *V = voice;
-      l = r = 0.0f;		
+      VOICE* V = voice;
+      l = r = 0.0f;
       for (v = 0; v < track_count; v++) {
-	if (V->note == 0) {
-	  V++;
-	  continue;
-	}
-	something_done = true;
-	V->frac += V->delta;  //integer-based linear interpolation
-	V->pos += V->frac >> 16;
-	V->frac &= 0xFFFF;
-	if (V->pos > V->end) {
-	  V->pos -= V->loop;
-	}
-	i = waves[V->pos] + ((V->frac * (waves[V->pos + 1] - waves[V->pos])) >> 16);
-	x = V->env * (float)i / 32768.0f;				
-	V->env = V->env * V->dec;  //envelope
-	if (x > 0.0f) { 
-	  x -= od * x * x;
-	  if (x < -V->env) {
-	    x = -V->env; 
-	  }
-	}			
-	l += V->outl * x;
-	r += V->outr * x;
-	V++;
+        if (V->note == 0) {
+          V++;
+          continue;
+        }
+        something_done = true;
+        V->frac += V->delta;  //integer-based linear interpolation
+        V->pos += V->frac >> 16;
+        V->frac &= 0xFFFF;
+        if (V->pos > V->end) {
+          V->pos -= V->loop;
+        }
+        i = waves[V->pos] + ((V->frac * (waves[V->pos + 1] - waves[V->pos])) >> 16);
+        x = V->env * (float)i / 32768.0f;
+        V->env = V->env * V->dec;  //envelope
+        if (x > 0.0f) {
+          x -= od * x * x;
+          if (x < -V->env) {
+            x = -V->env;
+          }
+        }
+        l += V->outl * x;
+        r += V->outr * x;
+        V++;
       }
       tl += tfrq * (l - tl);  //treble boost
       tr += tfrq * (r - tr);
-      r  += treb * (r - tr);
-      l  += treb * (l - tl);			
+      r += treb * (r - tr);
+      l += treb * (l - tl);
       lfo0 += dlfo * lfo1;  //LFO for tremolo and autopan
       lfo1 -= dlfo * lfo0;
       l += l * lmod * lfo1;
@@ -313,12 +308,12 @@ bool LunarEpiano::process_stereo(float **pin, float **pout, int n, int mode)
       *out1++ = r;
     }
     if (frame < n) {
-      if (track_count == 0 && param[4] > 0.5f) { 
-	lfo0 = -0.7071f;  
-	lfo1 = 0.7071f; 
+      if (track_count == 0 && param[4] > 0.5f) {
+        lfo0 = -0.7071f;
+        lfo1 = 0.7071f;
       } //reset LFO phase - good idea?
       long note = notes[event++];
-      long vel  = notes[event++];
+      long vel = notes[event++];
       long voice = notes[event++];
       noteOn(note, vel, voice);
     }
@@ -328,7 +323,7 @@ bool LunarEpiano::process_stereo(float **pin, float **pout, int n, int mode)
   }
   if (fabs(tr) < 1.0e-10) {
     tr = 0.0f;
-  }	
+  }
   for (v = 0; v < track_count; v++) {
     if (voice[v].env < SILENCE) {
       voice[v].note = 0;
@@ -338,8 +333,7 @@ bool LunarEpiano::process_stereo(float **pin, float **pout, int n, int mode)
   return something_done;
 }
 
-const char *LunarEpiano::describe_value(int param, int value) 
-{
+const char* LunarEpiano::describe_value(int param, int value) {
   static char txt[20];
   sprintf(txt, "%.2f", value * 0.001f);
   return txt;
