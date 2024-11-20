f2c27c4832a9b208079acf4cc54dce9362066e8a
Tim Hunt <tnhunt@gmail.com>
2024-11-20 02:47:40 +0000

    useful changes to some utils

--- src/neil/utils/base.py
+++ src/neil/utils/base.py
@@ -4,17 +4,36 @@ import os, sys, re
 
 
 class Vec2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y=None):
+        if isinstance(x, Vec2):
+            self.x = x.x
+            self.y = x.y
+        else:
+            self.x = x
+            self.y = y if y is not None else x
 
-    def move_to(self, x, y):
-        self.x = x
-        self.y = y
+    def set(self, x, y=None):
+        if isinstance(x, Vec2):
+            self.x = x.x
+            self.y = x.y
+        else:
+            self.x = x
+            self.y = y if y is not None else x
+        return self
 
-    def move_by(self, x, y):
-        self.x += x
-        self.y += y
+    def move_by(self, x, y=None):
+        if isinstance(x, Vec2):
+            self.x += x.x
+            self.y += x.y
+        else:
+            self.x += x
+            self.y += y if y is not None else x
+        return self
+    
+    def ints(self):
+        self.x = int(self.x)
+        self.y = int(self.y)
+        return self
 
     def __sub__(self, o):
         if isinstance(o, Vec2):
@@ -43,74 +62,106 @@ class Vec2:
     def __str__(self):
         return f"Vec2({self.x}, {self.y})"
     
-    def eq(self, o):
+    def __eq__(self, o):
         return self.x == o.x and self.y == o.y
-
-
     
+    def __iter__(self):
+        yield from (self.x, self.y)
+
+
+# def __init__(self, pos = None, size = None):
+# self.pos = pos if pos is not None else Vec2(0, 0)
+# self.size = size if size is not None else Vec2(0, 0)
 # this is the abstract canvas area in made up dimensions , not the screen pos in pixels
 class Area:
-    def __init__(self, pos = None, size = None):
-        self._pos = pos if pos is not None else Vec2(0, 0)
-        self._size = size if size is not None else Vec2(0, 0)
+    def __init__(self, first = None, second = None, x2 = None, y2 = None):
+        if isinstance(first, Area):
+            self.pos = Vec2(first.pos)
+            self.size = Vec2(first.size)
+        elif isinstance(first, Vec2) and isinstance(second, Vec2):
+            self.pos = Vec2(first)
+            self.size = Vec2(second)
+        elif first is not None and second is not None and x2 is not None and y2 is not None:
+            self.pos = Vec2(first, second)
+            self.size = Vec2(x2, y2)
+        else:
+            self.pos = Vec2(0, 0)
+            self.size = Vec2(0, 0)
 
     # copies Area or gtk.Rectangle
-    def copy(self, rect):
-        self._pos.move_to(rect.x, rect.y)
-        self._size.move_to(rect.width, rect.height)
+    def set(self, rect):
+        self.pos.set(rect.x, rect.y)
+        self.size.set(rect.width, rect.height)
         return self
     
+    def __mul__(self, o):
+        return Area(self.pos * o, self.size * o)
+    
+    def __truediv__(self, o):
+        return Area(self.pos / o, self.size / o)
+        
     # property access so x,y,width, height access properties of area
     @property
     def x(self):
-        return self._pos.x
-    
+        return self.pos.x
+
     @property
     def y(self):
-        return self._pos.y
-    
+        return self.pos.y
+
     @property
     def width(self):
-        return self._size.x
-    
+        return self.size.x
+
     @property
     def height(self):
-        return self._size.y
-    
+        return self.size.y
+
+    def get_mid(self):
+        return Vec2(self.x + self.width / 2, self.y + self.height / 2)
+
     def is_empty(self):
-        return self._size.x == 0 or self._size.y == 0
+        return self.size.x == 0 or self.size.y == 0
 
     def contains(self, x, y):
-        return (self._pos.x <= x <= self._pos.x + self._size.x and 
-                self._pos.y <= y <= self._pos.y + self._size.y)
+        return (self.pos.x <= x <= self.pos.x + self.size.x and 
+                self.pos.y <= y <= self.pos.y + self.size.y)
 
-    def get_size(self):
-        return self._size
     
-    def __str__(self):
-        return f"Area({self._pos}, {self._size})"
-    
-    def eq(self, o):
-        return self._pos.eq(o._pos) and self._size.eq(o._size)
-
-class OffsetArea(Area):
-    def __init__(self, pos, offset, size):
-        super().__init__(pos, size)
-        self._offset = offset
-
-    @property
-    def x(self):
-        return self._pos.x + self._offset.x
-
-    @property
-    def y(self):
-        return self._pos.y + self._offset.y
-
-    def contains(self, x, y):
-        return super().contains(x - self._offset.x, y - self._offset.y)
+    def ints(self):
+        self.pos.ints()
+        self.size.ints()
+        return self
 
     def __str__(self):
-        return f"OffsetArea(pos={self._pos}, offset={self._offset}, size={self._size})"
+        return f"Area({self.pos}, {self.size})"
+
+    def __eq__(self, o):
+        return (self.x == o.x and self.y == o.y and 
+               self.width == o.width and self.height == o.height)
+
+
+    
+
+
+# class OffsetArea(Area):
+#     def __init__(self, pos, offset, size):
+#         super().__init__(pos, size)
+#         self._offset = offset
+
+#     @property
+#     def x(self):
+#         return self._pos.x + self._offset.x
+
+#     @property
+#     def y(self):
+#         return self._pos.y + self._offset.y
+
+#     def contains(self, x, y):
+#         return super().contains(x - self._offset.x, y - self._offset.y)
+
+#     def __str__(self):
+#         return f"OffsetArea(pos={self._pos}, offset={self._offset}, size={self._size})"
 
 # it can scale for hidpi &
 # calculate sizes based on earlier values using parse_size())
@@ -132,32 +183,42 @@ class Sizes():
     def __init__(self, defaults = {'margin' : 6}, **kwargs):
         self.__defaults = defaults
         self.__init_args = kwargs
-        self.__scale = 1.0
+        # self.__scale = 1.0
         self.__values = []
         self.set_values(kwargs)
 
-    def set_scale(self, new_scale):
-        self.__scale = new_scale
-        self.set_values(self.__init_args)
+    # def set_scale(self, new_scale):
+    #     self.__scale = new_scale
+    #     self.set_values(self.__init_args)
 
-    def get_scale(self, new_scale):
-        return self.__scale
+    # def get_scale(self, new_scale):
+    #     return self.__scale
     
     def __getitem__(self, name):
-        return self.get(name)
-    
-    def get(self, name) -> float:
         if name in self.__values:
-            # scale tuples
-            if isinstance(self.__values[name], Vec2):
-                return Vec2(
-                    self.__values[name].x * self.__scale, 
-                    self.__values[name].y * self.__scale
-                )
-            elif isinstance(self.__values[name], (list, tuple)):
-                return [v * self.__scale for v in self.__values[name]]
-            else:
-                return self.__values[name] * self.__scale
+            return self.__values[name]
+    
+    def get(self, name):
+        if name in self.__values:
+            return self.__values[name]
+            # if isinstance(self.__values[name], Vec2):
+            #     return Vec2(
+            #         self.__values[name].x * self.__scale, 
+            #         self.__values[name].y * self.__scale
+            #     )
+            # elif isinstance(self.__values[name], Area):
+            #     return Area(
+            #         self.__values[name].pos.x * self.__scale, 
+            #         self.__values[name].pos.y * self.__scale, 
+            #         self.__values[name].size.x * self.__scale, 
+            #         self.__values[name].size.y * self.__scale
+            #     )
+            # elif isinstance(self.__values[name], list):
+            #     return [v * self.__scale for v in self.__values[name]]
+            # elif isinstance(self.__values[name], tuple):
+            #     return (v * self.__scale for v in self.__values[name])
+            # else:
+            #     return self.__values[name] * self.__scale
     
     def half(self, name):
         return self.get(name) / 2
@@ -178,6 +239,13 @@ class Sizes():
             return [self.parse_value(v) for v in value]
         elif isinstance(value, Vec2):
             return Vec2(self.parse_value(value.x), self.parse_value(value.y))
+        elif isinstance(value, Area):
+            return Area(
+                self.parse_value(value.pos.x), 
+                self.parse_value(value.pos.y), 
+                self.parse_value(value.size.x), 
+                self.parse_value(value.size.y)
+            )
         elif isinstance(value, float):
             return value
         else:
@@ -187,7 +255,7 @@ class Sizes():
     # treat text as the property name of an object in self.__values
     # treat numbers as the index of a list in self.__values  
     def parse_size(self, equation:str) -> int | float:
-        match_pattern = re.compile(r'(\w+)?')
+        match_pattern = re.compile(r'([\w\.]+)?')
         matches = match_pattern.findall(equation)
 
         for match in matches:
@@ -199,10 +267,13 @@ class Sizes():
             
             if '.' in key:
                 parts = key.split('.')
+
                 value = self.__values
                 for part in parts:
-                    if part.isdigit():
+                    if part.isdigit() and isinstance(value, (list, tuple)):
                         value = value[int(part)]
+                    elif hasattr(value, part):
+                        value = getattr(value, part)
                     else:
                         value = value.get(part, 0)
                 equation = re.sub(r'\b{}\b'.format(re.escape(match)), str(value), equation)
