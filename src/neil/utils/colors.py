d77eb9b6a0cb653acbeb480bde8f5d6d39f15538
Tim Hunt <tnhunt@gmail.com>
2024-11-20 02:48:20 +0000

    slightly improved color theme handling

--- src/neil/utils/colors.py
+++ src/neil/utils/colors.py
@@ -1,209 +1,253 @@
-from neil.utils import PluginType, plugin_color_schemes
-
-
+from .plugin import PluginType, get_plugin_type
 import zzub
 
 
-# class ColorTypes:
-#     DEFAULT = 0
-#     MUTED = 1
-#     LED_OFF = 2
-#     LED_ON = 3
-#     LED_BORDER = 4
-#     LED_WARNING = 7
-#     CPU_OFF = 2
-#     CPU_ON = 3
-#     CPU_BORDER = 4
-#     CPU_WARNING = 4
-#     BORDER_IN = 8
-#     BORDER_OUT = 8
-#     BORDER_SELECT = 8
-#     TEXT = 9
-
-class ColorType:
-    Default = 0
-    Muted = 1
-    LedOff = 3
-    LedOn = 2
-    IndicatorBg = 4
-    IndicatorFg = 5
-    IndicatorBorder = 6
-    IndicatorWarning = 7
-    Border = 8
-    Text = 9
-    Background = 10
-    Line = 11
+plugin_color_group = {
+    PluginType.Root: "Master",
+    PluginType.Instrument: "Generator",
+    PluginType.Effect: "Effect",
+    PluginType.CV: "Controller",
+    PluginType.Controller: "Effect",
+    PluginType.Streamer: "Generator",
+    PluginType.Other: "Effect",
+}
 
 
-color_keys = [
-    'MV ${PLUGIN}',
-    'MV ${PLUGIN} Mute',
-    'MV ${PLUGIN} LED Off',
-    'MV ${PLUGIN} LED On',
-    'MV Indicator Background',
-    'MV Indicator Foreground',
-    'MV Indicator Border',
-    'MV Indicator Warning',
-    'MV Border',
-    'MV Text',
-    'MV Background',
-    'MV Line'
+plugin_led_group = {
+    PluginType.Root: "Master",
+    PluginType.Instrument: "Generator",
+    PluginType.Effect: "Effect",
+    PluginType.CV: "Generator",
+    PluginType.Controller: "Effect",
+    PluginType.Streamer: "Effect",
+    PluginType.Other: "Effect",
+}
+
+
+def get_plugin_color_key(plugin: zzub.Pluginloader | zzub.Plugin, suffix: str | bool=False):
+    if suffix:
+        return plugin_color_group[get_plugin_type(plugin)] + " " + suffix.strip()
+    else:
+        return plugin_color_group[get_plugin_type(plugin)]
+    
+
+# 'get_machine_color_key',
+# 'plugin_color_group',
+# 'plugin_led_group',
+# 'get_plugin_type_name',
+
+# def get_plugin_type_name(plugin: zzub.Pluginloader | zzub.Plugin):
+
+def get_plugin_color_group(plugin: zzub.Pluginloader | zzub.Plugin):
+    return plugin_color_group[get_plugin_type(plugin)] 
+
+theme_properties = [
+    # machine view/router colors
+    "MV Background", 
+    "MV Text",
+
+    "MV Line",
+
+    "MV Amp Handle", 
+    "MV Amp BG", 
+
+    "MV Border",
+    # "MV Machine Border",    
+    # "MV Selected Border", 
+
+    "MV Arrow",             "MV Arrow Border In",            "MV Arrow Border Out",
+    "MV Controller Arrow",  "MV Controller Arrow Border In", "MV Controller Arrow Border Out", 
+
+    "MV Generator",        "MV Generator Mute",
+    "MV Effect",           "MV Effect Mute",
+    "MV Master",           "MV Master Mute",
+    "MV Controller",       "MV Controller Mute",
+
+    "MV Generator LED Off", "MV Generator LED On",
+    "MV Effect LED Off",    "MV Effect LED On",
+    "MV Master LED Off",    "MV Master LED On",
+
+
+
+    # Pattern colors
+    "PE BG Very Dark", "PE BG Dark", "PE BG", "PE BG Light", "PE BG Very Light",
+
+    "PE Sel BG",
+    "PE Text",
+    "PE Track Numbers",
+    "PE Row Numbers",
+
+    # seqencer colors
+    "SE Background",
+    "SA Amp BG", "SA Amp Line",
+    "SA Freq BG", "SA Freq Line",
+    "SE Weak Line","SE Strong Line",
+    "SE Text",
+    "SE Loop Line",
+    "SE Track Background",
+
+    # "WE BG",
+    # "WE Line",
+    # "WE Fill",
+    # "WE Peak Fill",
+    # "WE Grid",
+    # "WE Selection",
+    # "WE Stretch Cue",
+    # "WE Split Bar",
+    # "WE Slice Bar",
+    # "WE Wakeup Peaks",
+    # "WE Sleep Peaks",
+
+    # "EE Line",
+    # "EE Fill",
+    # "EE Dot",
+    # "EE Sustain",
+    # "EE Dot Selected",
+    # "EE BG",
+    # "EE Grid"
 ]
 
-# this has the colour options from 
-class RouterColors:
-    def __init__(self, config):
-        self.config = config
-
-        # one of neil.utils.PluginType constant
-        self.plugin_type = None
-
-        # each scheme has the colors listed in color_keys
-        self.plugin_scheme = None
-
-        # all the colors schemes
-        self.plugin_schemes = None
-
-        #either zzub.zzub_connection_type_cv or zzub_connection_type_audio
-        self.arrow_type = None
-        #
-        self.arrow_colors = None
-        
-        self.refresh()
-        
-
-    def refresh(self):
-        self.weight_rgb = (1,1,1)
-        self.plugin_schemes = self.rebuild_plugin_colors()
-        self.arrow_colors = self.rebuild_arrow_colors()
-        self.set_plugin_type(PluginType.Instrument)
 
 
-    def rebuild_plugin_colors(self):
-        all_schemes = []
-        for plugintype, name in plugin_color_schemes.items():
-            plugin_scheme = []
-            for name in [x.replace('${PLUGIN}', name) for x in color_keys]:
-                plugin_scheme.append(self.config.get_float_color(name))
-            all_schemes.append(plugin_scheme)
+class BaseColors:
+    def shade(self, color, weight, *args, **kwargs):
+        if isinstance(color, str):
+            if color in self.colors:
+                color = self.colors[color]
 
-        return all_schemes
-    
-
-    def rebuild_arrow_colors(self):
-        return [
-            [
-                self.config.get_float_color("MV Controller Arrow"),
-                self.config.get_float_color("MV Controller Arrow Border In"),
-                self.config.get_float_color("MV Controller Arrow Border Out"),
-            ],
-            [
-                self.config.get_float_color("MV Arrow"),
-                self.config.get_float_color("MV Arrow Border In"),
-                self.config.get_float_color("MV Arrow Border Out"),
-            ],
-        ]
-
-
-    def set_plugin_type(self, plugin_type: PluginType):
-        self.plugin_type = plugin_type
-        self.plugin_scheme = self.plugin_schemes[plugin_type]
-
-
-    def set_audio_connection(self, is_audio):
-        self.arrow_type = zzub.zzub_connection_type_audio if is_audio else zzub.zzub_connection_type_cv
-        self.arrow_scheme = self.arrow_schemes[self.arrow_type]
-
-    def default(self, is_muted, weight = None):
-        color_name = ColorType.Muted if is_muted else ColorType.Default
-
-        if weight is None:
-            return self.plugin_scheme[color_name]
-        else:
-            return self.shade(color_name, weight)
-
-
-    #
-    def shade(self, color, weight):
-        if isinstance(color, int):
-            color = self.plugin_scheme[color]
-
-        if weight >= 0:
-             # lighten rgb color
+        if weight >= 0:       # lighter rgb color
+            weight = min(1, weight)
             return (col + (1 - col) * weight for col in color)
-        else:
-            weight = -weight
-            return (col * weight for col in color)
-
-    def arrow(self, is_audio, amp=1):
-        return self.shade(self.arrow_colors[is_audio][0], -1 + amp)
-
-    def background(self):
-        return self.plugin_scheme[ColorType.Background]
-    
-    def line(self):
-        return self.plugin_scheme[ColorType.Line]
-
-    def arrow_in(self, is_audio, amp=1):
-        return self.shade(self.arrow_colors[is_audio][1], -1 + amp)
+        else:                  # darker rgb color
+            weight = -max(-1, weight)
+            return (col - (col * weight) for col in color)
 
 
-    def arrow_out(self, is_audio, amp=1):
-        return self.shade(self.arrow_colors[is_audio][2], -1 + amp)
 
 
-    def plugin(self):
-        return self.plugin_scheme[ColorType.Default]
-    
 
-    def muted(self):
-        return self.plugin_scheme[ColorType.Muted]
+class RouterColors(BaseColors):
+    def __init__(self, colors):
+        self.colors = colors
 
+    def amp(self, is_handle):
+        return self.colors["MV Amp Handle" if is_handle else "MV Amp BG"]
 
-    def led_on(self):
-        return self.plugin_scheme[ColorType.LedOn]
+    def arrow(self, is_audio, is_border=False, border_direction=False):
+        prefix = "MV Arrow" if is_audio else "MV Controller Arrow"
+        suffix = (" Border In" if border_direction else " Border Out") if is_border else ""
+        return self.colors[prefix + suffix]
 
-
-    def led_off(self):
-        return self.plugin_scheme[ColorType.LedOff]
-
-
-    def led_warning(self):
-        return self.plugin_scheme[ColorType.IndicatorWarning]
-    
-
-    def cpu_on(self):
-        return self.plugin_scheme[ColorType.LedOn]
-
-
-    def cpu_off(self):
-        return self.plugin_scheme[ColorType.LedOff]
-
-
-    def cpu_border(self):
-        return self.plugin_scheme[ColorType.IndicatorBg]
-
-
-    def cpu_warning(self):
-        return self.plugin_scheme[ColorType.IndicatorWarning]
-
-
-    def border(self):
-        return self.plugin_scheme[ColorType.Border]
-
-
-    def border_in(self):
-        return self.plugin_scheme[ColorType.Border]
-
-
-    def border_out(self):
-        return self.plugin_scheme[ColorType.Border]
-
+    def plugin(self, type, is_selected=False, is_muted=False):
+        prefix = "MV " + plugin_color_group[type]
+        suffix = " Mute" if is_muted else ""
+        key = prefix + suffix
+        return self.shade(key, 0.2) if is_selected else self.colors[key]
 
     def text(self):
-        return self.plugin_scheme[ColorType.Text]
+        return self.colors["MV Text"]
+
+    def plugin_border(self, type, is_selected=False):
+        key = "MV " + plugin_color_group[type]
+        shade = -0.2 if is_selected else -0.5
+        return self.shade(key, shade)
+
+    def border(self):
+        return self.colors["MV Border"]
+    
+    #state 0=off, 1=on 2=warning
+    def led(self, type, state):
+        if state == 2:
+            return self.colors["MV Machine LED Warning"]
+
+        prefix = "MV " + plugin_led_group[type]
+        suffix = " LED On" if state else " LED Off"
+        return self.colors[prefix + suffix]
+    
+    def background(self):
+        return self.colors["MV Background"]
+
+    def line(self):
+        return self.colors["MV Line"]
+    
+
+
+class PatternColors(BaseColors):
+    shades = ["PE BG Very Dark", "PE BG Dark", "PE BG", "PE BG Light", "PE BG Very Light"]
+
+    def __init__(self, colors):
+        self.colors = colors
+
+
+    def background(self, shading=0, is_selected=False):
+        if is_selected:
+            return self.colors["PE Sel BG"]
+        
+        shading = max(-2, min(2, shading)) + 2
+        shade = PatternColors.shades[shading]
+        
+        return self.colors[shade]
+
+    def text(self, is_selected=False):
+        return self.colors["PE Text"]
+
+    def track_numbers(self):
+        return self.colors["PE Track Numbers"]
+
+    def row_numbers(self):
+        return self.colors["PE Row Numbers"]
+
+
+
+class SequencerColors(BaseColors):
+    line_shades = ["SE Weak Line", "SE Strong Line"]
+
+    def __init__(self, colors):
+        self.colors = colors
+
+    #darknen = 0 to +2
+    def background(self, darknen=0, is_selected=False):
+        if is_selected:
+            darken = 3
+        
+        shade = min(0, 3)
+        
+        return self.shade("SE Background", darken)
+#
+    def text(self):
+        return self.colors["SE Text"]
+
+    def loop_line(self):
+        return self.colors["SE Loop Line"]
+
+    def track_background(self):
+        return self.colors["SE Track Background"]
+    
+
+    def line(self, is_light: False):
+        strength_name = PatternColors.line_shades[bool(is_light)]
+        return self.colors[strength_name]
+
+
+class Colors(BaseColors):
+    def __init__(self, config):
+        self.config = config
+        self.colors = {}
+        self.router = RouterColors(self.colors)
+        self.sequencer = SequencerColors(self.colors)
+        self.pattern = PatternColors(self.colors)
+        self.refresh()
+
+    def refresh(self):
+        for color in theme_properties:
+            self.colors[color] = self.config.get_float_color(color)
 
 
 __all__ = [
-    'RouterColors'
+    'Colors',
+    'PatternColors',
+    'SequencerColors',
+    'RouterColors',
+    'get_plugin_color_key',
+    'get_plugin_color_group',
 ]
